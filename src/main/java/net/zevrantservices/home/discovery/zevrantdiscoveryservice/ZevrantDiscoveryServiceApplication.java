09a7776a558eb6a5d32b822a369c49b9e1738a99
Zevrant <gerethd@gmail.com>
2019-03-17 08:29:19 -0400

    initial commit

---
+++ src/main/java/net/zevrantservices/home/discovery/zevrantdiscoveryservice/ZevrantDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package net.zevrantservices.home.discovery.zevrantdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZevrantDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZevrantDiscoveryServiceApplication.class, args);
+    }
+
+}
